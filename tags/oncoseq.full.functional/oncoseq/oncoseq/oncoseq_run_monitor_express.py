5959a72ec8336cf789564649803ade69b73389c0
alebalbin <alebalbin@3deb8bab-1f0f-d856-ab53-30b0a50da13b>
2012-11-13 16:37:48 +0000

    This version of oncoseq is full functional for analyzing exome, and transcriptome data. This version this not contain yet modifications to incorporate tophat fusions, tumor content estimation and varscan calling of copy number.

---
+++ tags/oncoseq.full.functional/oncoseq/oncoseq/oncoseq_run_monitor_express.py
@@ -0,0 +1,60 @@
+'''
+Created on Nov 3, 2012
+
+@author: oabalbin
+'''
+import logging
+import argparse
+import sqlite3
+import os
+
+from oncoseq.lib import rundb
+#from oncoseq.rnaseq.rnaseq_copy_remote import copy_remote
+from oncoseq.lib.copy_remote_files_express import copy_remote
+
+
+def monitor_jobs(db, config_file, server_name, output_dir, ssh_port, overwrite):
+    conn = sqlite3.connect(db)
+    conn.row_factory = sqlite3.Row    
+    c = conn.cursor()
+    job_dict = rundb.selectall(c)
+    c.close()
+    for job_name,status in job_dict.iteritems():
+        if status == rundb.STATUS_DONE:
+            continue
+        logging.info("Monitoring analysis %s current status=%s" % (job_name,status))
+        # ssh_port=ssh_port,overwrite=overwrite
+        success = copy_remote(output_dir, config_file, server_name, job_name,
+                              #ssh_port, 
+                              overwrite)
+        
+        # In this script you do not want to close conecction
+        # If a particular job did not finished.
+        #if success:
+        #    rundb.setstatus(db, job_name, rundb.STATUS_DONE)
+        if not success:
+            logging.error("Job %s missing 'analysis.xml' file, job might need to be re-started" % (job_name))
+
+def main():
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ssh-port", dest="ssh_port", type=int, default=22)
+    parser.add_argument("--overwrite", dest="overwrite", action="store_true", default=False)
+    parser.add_argument("db_file")
+    parser.add_argument("config_file")
+    parser.add_argument("server_name")
+    parser.add_argument("output_dir")
+    args = parser.parse_args()
+    # create db
+    if not os.path.exists(args.db_file):
+        rundb.create_db(args.db_file)
+    monitor_jobs(args.db_file,
+                 args.config_file,
+                 args.server_name,
+                 args.output_dir,
+                 args.ssh_port,
+                 args.overwrite)
+
+if __name__ == '__main__':
+    main()
